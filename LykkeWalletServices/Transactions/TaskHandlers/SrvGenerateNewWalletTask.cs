9acc6c705fa83f4afa33c2f7ff6a4b75ed388921
bijakatlykkex <bijakatlykkex@gmail.com>
2015-12-26 21:54:25 +0330

    Contract handling for generate new wallet improved.

--- LykkeWalletServices/Transactions/TaskHandlers/SrvGenerateNewWalletTask.cs
+++ LykkeWalletServices/Transactions/TaskHandlers/SrvGenerateNewWalletTask.cs
@@ -16,30 +16,33 @@ namespace LykkeWalletServices.Transactions.TaskHandlers
             this.network = network;
             this.exchangePrivateKey = exchangePrivateKey;
         }
-        
+
         public async Task<Tuple<GenerateNewWalletTaskResult, Error>> ExecuteTask()
         {
             GenerateNewWalletTaskResult result = null;
+            string walletAddress = null;
+            string walletPrivateKey = null;
+            string multiSigAddressStorage = null;
             Error error = null;
             try
             {
                 Key key = new Key();
                 BitcoinSecret secret = new BitcoinSecret(key, this.network);
-                result = new GenerateNewWalletTaskResult();
-                result.WalletAddress = secret.GetAddress().ToWif();
-                result.WalletPrivateKey = secret.PrivateKey.GetWif(network).ToWif();
+
+                walletAddress = secret.GetAddress().ToWif();
+                walletPrivateKey = secret.PrivateKey.GetWif(network).ToWif();
 
                 var multiSigAddress = PayToMultiSigTemplate.Instance.GenerateScriptPubKey(2, new PubKey[] { secret.PubKey ,
                 (new BitcoinSecret(exchangePrivateKey, network)).PubKey });
-                result.MultiSigAddress = multiSigAddress.GetScriptAddress(network).ToString();
+                multiSigAddressStorage = multiSigAddress.GetScriptAddress(network).ToString();
 
                 using (SqliteLykkeServicesEntities entitiesContext = new SqliteLykkeServicesEntities())
                 {
                     KeyStorage item = new KeyStorage
                     {
-                        WalletAddress = result.WalletAddress,
-                        WalletPrivateKey = result.WalletPrivateKey,
-                        MultiSigAddress = result.MultiSigAddress,
+                        WalletAddress = walletAddress,
+                        WalletPrivateKey = walletPrivateKey,
+                        MultiSigAddress = multiSigAddressStorage,
                         ExchangePrivateKey = exchangePrivateKey,
                         MultiSigScript = multiSigAddress.ToString(),
                         Network = (network == Network.Main ? NetworkType.Main : NetworkType.TestNet).ToString()
@@ -48,6 +51,11 @@ namespace LykkeWalletServices.Transactions.TaskHandlers
                     entitiesContext.KeyStorages.Add(item);
                     await entitiesContext.SaveChangesAsync();
                 }
+
+                result = new GenerateNewWalletTaskResult();
+                result.WalletAddress = walletAddress;
+                result.WalletPrivateKey = walletPrivateKey;
+                result.MultiSigAddress = multiSigAddressStorage;
             }
             catch (Exception e)
             {
