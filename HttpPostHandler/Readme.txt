da08d554ba7d1060ccc5cb2cd025580ccf9dd8f7
bijakatlykkex <bijakatlykkex@gmail.com>
2016-06-23 19:15:19 +0430

    Some tests documentation and material added.

---
+++ HttpPostHandler/Readme.txt
@@ -0,0 +1,2 @@
+﻿This is the dummy listener used to replace the LykkeJobs for testing; It is called when after broadcasting transaction to bitcoin network, LykkeJobs is notified.
+It should response to with no error: curl -X Post http://localhost:8088/HandledTx -H "Content-Type: application/json" -d "{\"TransactionId\" : \"transactionId\", \"BlockchainHash\" : \"blockchainHash\",\"Operation\" : \"Transfer\" }"
---
+++ HttpPostHandler/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+[assembly: OwinStartup(typeof(HttpPostHandler.Startup))]
+
+namespace HttpPostHandler
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // This code configures Web API. The Startup class is specified as a type
+            // parameter in the WebApp.Start method.
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings =
+            new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
