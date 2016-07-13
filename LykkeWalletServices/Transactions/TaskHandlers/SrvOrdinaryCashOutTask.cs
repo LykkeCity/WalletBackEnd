LykkeCity/WalletBackEnd

commit 6d5bc83a0ef3a8538bdd8101bf1bb388c07bb88a

A float multiplication BugFix

LykkeWalletServices/Transactions/TaskHandlers/SrvOrdinaryCashOutTask.cs
@@ -25,7 +25,7 @@ namespace LykkeWalletServices.Transactions.TaskHandlers
                 using (SqlexpressLykkeEntities entities = new SqlexpressLykkeEntities(ConnectionString))
                 {
                     OpenAssetsHelper.GetScriptCoinsForWalletReturnType walletCoins = (OpenAssetsHelper.GetScriptCoinsForWalletReturnType)
-                        await OpenAssetsHelper.GetCoinsForWallet(data.MultisigAddress, !OpenAssetsHelper.IsRealAsset(data.Currency) ? Convert.ToInt64(data.Amount * OpenAssetsHelper.BTCToSathoshiMultiplicationFactor) : 0, data.Amount, data.Currency,
+                        await OpenAssetsHelper.GetCoinsForWallet(data.MultisigAddress, !OpenAssetsHelper.IsRealAsset(data.Currency) ? Convert.ToInt64(data.Amount * OpenAssetsHelper.BTCToSathoshiMultiplicationFactor) : 0, Convert.ToInt64(data.Amount), data.Currency,
                         Assets, Network, Username, Password, IpAddress, ConnectionString, entities, false);
                     if (walletCoins.Error != null)
                     {

Core/IQueueReader.cs
@@ -143,7 +143,7 @@ namespace Core
     {
         public string MultisigAddress { get; set; }
         // ToDo - At first we assume the currency is not divisable
-        public float Amount { get; set; }
+        public double Amount { get; set; }
         public string Currency { get; set; }
         public string PrivateKey { get; set; }
         public string PublicWallet { get; set; }

Core/IQueueWriter.cs
@@ -127,7 +127,7 @@ namespace Core
     public class GetCurrentBalanceTaskResultElement
     {
         public string Asset { get; set; }
-        public float Amount { get; set; }
+        public double Amount { get; set; }
     }
 
     public class GetFeeOutputsStatusTaskResult : ITaskResult

LykkeWalletServices/OpenAssetsHelper.cs
@@ -469,7 +469,7 @@ namespace LykkeWalletServices
             }
         }
 
-        public static float GetAssetBalance(UniversalUnspentOutput[] outputs,
+        public static double GetAssetBalance(UniversalUnspentOutput[] outputs,
             string assetId, long multiplyFactor, Func<int> getMinimumConfirmationNumber = null)
         {
             float total = 0;
@@ -554,11 +554,11 @@ namespace LykkeWalletServices
         }
 
         public static bool IsAssetsEnough(UniversalUnspentOutput[] outputs,
-            string assetId, float assetAmount, long multiplyFactor, Func<int> getMinimumConfirmationNumber = null)
+            string assetId, double assetAmount, long multiplyFactor, Func<int> getMinimumConfirmationNumber = null)
         {
             if (!string.IsNullOrEmpty(assetId))
             {
-                float total = GetAssetBalance(outputs, assetId, multiplyFactor, getMinimumConfirmationNumber);
+                double total = GetAssetBalance(outputs, assetId, multiplyFactor, getMinimumConfirmationNumber);
 
                 if (Math.Abs(total - assetAmount) >= 0)
                 {
@@ -581,7 +581,7 @@ namespace LykkeWalletServices
         }
 
         public static async Task<GetCoinsForWalletReturnType> GetCoinsForWallet
-            (string multiSigAddress, long requiredSatoshiAmount, float requiredAssetAmount, string asset, AssetDefinition[] assets,
+            (string multiSigAddress, long requiredSatoshiAmount, double requiredAssetAmount, string asset, AssetDefinition[] assets,
             Network network, string username, string password, string ipAddress, string connectionString, SqlexpressLykkeEntities entities,
             bool isOrdinaryReturnTypeRequired, bool isAddressMultiSig = true, Func<int> getMinimumConfirmationNumber = null)
         {

LykkeWalletServices/Transactions/TaskHandlers/SrvGetCurrentBalanceTask.cs
@@ -41,7 +41,7 @@ namespace LykkeWalletServices.Transactions.TaskHandlers
                 }
                 else
                 {
-                    float tempValue = GetAssetBalance(walletOuputs.Item1, "BTC", (long)BTCToSathoshiMultiplicationFactor, getMinimumConfirmationNumber);
+                    double tempValue = GetAssetBalance(walletOuputs.Item1, "BTC", (long)BTCToSathoshiMultiplicationFactor, getMinimumConfirmationNumber);
                     GetCurrentBalanceTaskResultElement element = new GetCurrentBalanceTaskResultElement();
                     element.Asset = "BTC";
                     element.Amount = tempValue;

Tests/GetCurrentBalanceTests.cs
@@ -23,7 +23,7 @@ namespace Lykkex.WalletBackend.Tests
     [TestFixture]
     public class GetCurrentBalanceTests : TaskTestsCommon
     {
-        public async static Task<float> GetAssetBalanceForMultisig(string multisig, string assetName, int confirmationNamber,
+        public async static Task<double> GetAssetBalanceForMultisig(string multisig, string assetName, int confirmationNamber,
             AzureStorage.AzureQueueExt QueueReader, AzureStorage.AzureQueueExt QueueWriter)
         {
             GetCurrentBalanceModel getCurrentBalance = new GetCurrentBalanceModel { TransactionId = "10", MultisigAddress = multisig, MinimumConfirmation = confirmationNamber };

commit d23a76dc11618611218f1b18792deb2ca2ccabb0

Fixed a bug introduced in commit 6d5bc83a0ef3a8538bdd8101bf1bb388c07bb88a

LykkeWalletServices/Transactions/TaskHandlers/SrvOrdinaryCashOutTask.cs
@@ -25,7 +25,7 @@ namespace LykkeWalletServices.Transactions.TaskHandlers
                 using (SqlexpressLykkeEntities entities = new SqlexpressLykkeEntities(ConnectionString))
                 {
                     OpenAssetsHelper.GetScriptCoinsForWalletReturnType walletCoins = (OpenAssetsHelper.GetScriptCoinsForWalletReturnType)
-                        await OpenAssetsHelper.GetCoinsForWallet(data.MultisigAddress, !OpenAssetsHelper.IsRealAsset(data.Currency) ? Convert.ToInt64(data.Amount * OpenAssetsHelper.BTCToSathoshiMultiplicationFactor) : 0, Convert.ToInt64(data.Amount), data.Currency,
+                        await OpenAssetsHelper.GetCoinsForWallet(data.MultisigAddress, !OpenAssetsHelper.IsRealAsset(data.Currency) ? Convert.ToInt64(data.Amount * OpenAssetsHelper.BTCToSathoshiMultiplicationFactor) : 0, data.Amount, data.Currency,
                         Assets, Network, Username, Password, IpAddress, ConnectionString, entities, false);
                     if (walletCoins.Error != null)
                     {