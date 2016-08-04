021db8790ae25efc0c9ef8415b0d1f754254ac88
bijakatlykkex <bijakatlykkex@gmail.com>
2016-08-04 19:39:45 +0430

    Added a method to get the version (608)

--- ServiceLykkeWallet/Controllers/GeneralController.cs
+++ ServiceLykkeWallet/Controllers/GeneralController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -16,6 +17,19 @@ namespace ServiceLykkeWallet.Controllers
 {
     public class GeneralController : ApiController
     {
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage GetVersion()
+        {
+            // With the help of http://stackoverflow.com/questions/909555/how-can-i-get-the-assembly-file-version
+
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+
+            var result = new HttpResponseMessage(HttpStatusCode.Accepted);
+            result.Content = new StringContent(version.ToString());
+
+            return result;
+        }
+
         [System.Web.Http.HttpGet]
         public async Task<HttpResponseMessage> FeeRate()
         {
