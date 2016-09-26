52b506019c9495a0d1132bdc640695570c2841a1
bijakatlykkex <bijakatlykkex@gmail.com>
2016-09-26 07:37:42 +0330

    Added logging to Private Key Add method.

--- ServiceLykkeWallet/Controllers/PrivateKeyController.cs
+++ ServiceLykkeWallet/Controllers/PrivateKeyController.cs
@@ -2,12 +2,15 @@
 using LykkeWalletServices;
 using LykkeWalletServices.Accounts;
 using NBitcoin;
+using Newtonsoft.Json;
 using ServiceLykkeWallet.Models;
 using System;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace ServiceLykkeWallet.Controllers
 {
@@ -15,18 +18,31 @@ namespace ServiceLykkeWallet.Controllers
     {
         // This should respond to curl -H "Content-Type: application/json" -X POST -d "{\"PrivateKey\":\"xyz\",\"IsP2PKH\":\"true\"}" http://localhost:8989/PrivateKey/Add
         [System.Web.Http.HttpPost]
-        public IHttpActionResult Add(AddPrivateKeyContract privatekey)
+        public async Task<IHttpActionResult> Add(AddPrivateKeyContract privatekey)
         {
+            IHttpActionResult result = null;
+
             try
             {
                 OpenAssetsHelper.AddPrivateKey(privatekey.PrivateKey, privatekey.IsP2PKH);
-                return Ok();
+                result = Ok();
 
             }
             catch (Exception ex)
             {
-                return InternalServerError(ex);
+                result = InternalServerError(ex);
             }
+
+            var pvPart01 = privatekey.PrivateKey.Substring(0, 5);
+            var pvPart02 = privatekey.PrivateKey.Substring(5);
+            Regex regex = new Regex("[a-zA-Z0-9]");
+            pvPart02 = regex.Replace(pvPart02, "x");
+            var pv = new AddPrivateKeyContract { IsP2PKH = privatekey.IsP2PKH, PrivateKey = pvPart01 + pvPart02 };
+            await OpenAssetsHelper.SendPendingEmailsAndLogInputOutput
+                (WebSettings.ConnectionString, "PrivateKeyAdd:" + JsonConvert.SerializeObject(pv),
+                TransactionsController.ConvertResultToString(result));
+
+            return result;
         }
     }
 }
