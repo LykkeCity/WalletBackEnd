10a817d06d54f5b559c77312b1b3fa4f0fd3deee
bijakatlykkex <bijakatlykkex@gmail.com>
2016-11-28 17:29:20 +0330

    Initial offchain support.

---
+++ ServiceLykkeWallet/Controllers/OffchainController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ServiceLykkeWallet.Controllers
+{
+    public class OffchainController: ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> AddClientProvidedChannelInput(string multiSig, string transactionId)
+        {
+            return Ok();
+        }
+
+        public async Task<IHttpActionResult> GenerateOnChainTx0(string clientMultiSig, string clientContributedTransactionHex,
+            string clientContributedAmount, string ChannelAssetName)
+        {
+            return Ok();
+        }
+
+        public async Task<IHttpActionResult> FinalizeChannelSetup(string channelId, string clientCommitment0)
+        {
+            return Ok();
+        }
+
+    }
+}
