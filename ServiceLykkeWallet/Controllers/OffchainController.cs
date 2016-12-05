e4edbdbaad37600ef6bdf55d0b158594db8eea3b
bijakatlykkex <bijakatlykkex@gmail.com>
2016-12-05 16:26:02 +0330

    Updated offchain method stubs

--- ServiceLykkeWallet/Controllers/OffchainController.cs
+++ ServiceLykkeWallet/Controllers/OffchainController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace ServiceLykkeWallet.Controllers
 {
-    public class OffchainController: ApiController
+    public class OffchainController : ApiController
     {
         [HttpGet]
         public async Task<IHttpActionResult> AddClientProvidedChannelInput(string multiSig, string transactionId)
@@ -15,17 +15,60 @@ namespace ServiceLykkeWallet.Controllers
             return Ok();
         }
 
-        public async Task<IHttpActionResult> GenerateChannelSetupTransaction(string ClientAddress, double ClientContributedContributedAmount,
-            string HubAddress, double HubContributedAmount, string ChannelAssetName)
-
+        public class UnsignedChannelSetupTransaction
         {
-            return Ok();
+            public string UnsigndTransaction
+            {
+                get;
+                set;
+            }
         }
 
-        public async Task<IHttpActionResult> FinalizeChannelSetup(string channelId, string clientCommitment0)
+        // http://localhost:8989/Offchain/GenerateUnsignedChannelSetupTransaction?ClientAddress=x&ClientContributedAmount=10&HubAddress=z&HubContributedAmount=10&ClientMultisigAddress=ab&ClientMultisigContributedAmount=10&ChannelAssetName=ac
+        [HttpGet]
+        public async Task<IHttpActionResult> GenerateUnsignedChannelSetupTransaction(string ClientAddress, double ClientContributedAmount,
+            string HubAddress, double HubContributedAmount, string ClientMultisigAddress, double ClientMultisigContributedAmount, string ChannelAssetName)
         {
-            return Ok();
+            return Json(new UnsignedChannelSetupTransaction { UnsigndTransaction = "0001" });
         }
 
+        public class UnsignedClientCommitmentTransactionResponse
+        {
+            public string FullySignedSetupTransaction
+            {
+                get;
+                set;
+            }
+
+            public string UnsignedClientCommitment0
+            {
+                get;
+                set;
+            }
+        }
+
+        // http://localhost:8989/Offchain/CreateUnsignedClientCommitmentTransaction?UnsignedChannelSetupTransaction=0001&ClientSignedChannelSetup=0001
+        [HttpGet]
+        public async Task<IHttpActionResult> CreateUnsignedClientCommitmentTransaction(string UnsignedChannelSetupTransaction, string ClientSignedChannelSetup)
+        {
+            return Json(new UnsignedClientCommitmentTransactionResponse { FullySignedSetupTransaction = "0002",
+                UnsignedClientCommitment0 = "0002" });
+        }
+
+        public class FinalizeChannelSetupResponse
+        {
+            public string SignedHubCommitment0
+            {
+                get;
+                set;
+            }
+        }
+
+        // http://localhost:8989/Offchain/FinalizeChannelSetup?FullySignedSetupTransaction=002&SignedClientCommitment0=002
+        [HttpGet]
+        public async Task<IHttpActionResult> FinalizeChannelSetup(string FullySignedSetupTransaction, string SignedClientCommitment0)
+        {
+            return Json(new FinalizeChannelSetupResponse { SignedHubCommitment0 = "0003" });
+        }
     }
 }
